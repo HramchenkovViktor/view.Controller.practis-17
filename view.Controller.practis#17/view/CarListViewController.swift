8ac6c0edf2fa669fc86ccd10357718434ee07d69
HramchenkovViktor <harmchenkovviktor@gmail.com>
2026-09-04 12:44:45 +0300

    первый лист

---
+++ view.Controller.practis#17/view/CarListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CarListViewController.swift
+//  view.Controller.practis#17
+//
+//  Created by Виктор on 04.09.2026.
+//
+
+import UIKit
+
+class CarListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ view.Controller.practis#17/view/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  view.Controller.practis#17
+//
+//  Created by Виктор on 04.09.2026.
+//
+
+import UIKit
+import SnapKit
+
+
+
+class ViewController: UIViewController {
+    
+    let titleLabel = UILabel()
+    let cardView = UIView()
+    let autoImage = UIImageView()
+    let titleCard = UILabel()
+    let selectedButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupConstraints()
+    }
+    
+    func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(titleLabel)
+        view.addSubview(cardView)
+        view.addSubview(selectedButton)
+        
+        cardView.addSubview(autoImage)
+        cardView.addSubview(titleCard)
+        
+        titleLabel.text = "Выбранный автомобиль"
+        titleLabel.textColor = .black
+        titleLabel.textAlignment = .center
+        titleLabel.numberOfLines = 0
+        titleLabel.clipsToBounds = true
+        
+        cardView.backgroundColor = .systemPink.withAlphaComponent(0.12)
+        cardView.clipsToBounds = true
+        cardView.layer.cornerRadius = 20
+        
+        
+        autoImage.image = UIImage(resource: .auto)
+        autoImage.layer.cornerRadius = 20
+        autoImage.clipsToBounds = true
+        autoImage.contentMode = .scaleAspectFill
+        
+        titleCard.text = "Автомобиль не выбран"
+        titleCard.textColor = .black
+        titleCard.textAlignment = .center
+        titleCard.numberOfLines = 0
+        titleCard.clipsToBounds = true
+        
+        selectedButton.setTitle("Выбрать автомобиль", for: .normal)
+        selectedButton.setTitleColor(.black, for: .normal)
+        selectedButton.layer.cornerRadius = 20
+        selectedButton.clipsToBounds = true
+        selectedButton.backgroundColor = .systemPink.withAlphaComponent(0.12)
+        selectedButton.addTarget(self, action: #selector(selectedButtonTapped), for: .touchUpInside)
+    }
+    
+    func setupConstraints() {
+        titleLabel.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        cardView.snp.makeConstraints{
+            $0.top.equalTo(titleLabel.snp.bottom).offset(15)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        autoImage.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(15)
+            $0.leading.trailing.equalToSuperview().inset(15)
+            $0.height.equalTo(180)
+        }
+        titleCard.snp.makeConstraints{
+            $0.top.equalTo(autoImage.snp.bottom).offset(15)
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(15)
+        }
+        selectedButton.snp.makeConstraints{
+            $0.top.equalTo(cardView.snp.bottom).offset(15)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(55)
+        }
+    }
+    @objc private func selectedButtonTapped(){
+        let viewController = CarListViewController()
+        
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+}
+
+
+
+
