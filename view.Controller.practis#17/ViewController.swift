6daa090b1cdbd6e30fe1ee496406a60b2f647840
HramchenkovViktor <harmchenkovviktor@gmail.com>
2026-09-04 12:18:22 +0300

    Initial Commit

---
+++ view.Controller.practis#17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  view.Controller.practis#17
+//
+//  Created by Виктор on 04.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
